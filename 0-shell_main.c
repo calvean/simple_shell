a6d57cb9fd89a096cd553dee20592bb191a3af5c
Krystian Maccs <krystianmaccs@gmail.com>
2022-08-31 04:32:11 -0700

    refactored header and main file

--- 0-shell_main.c
+++ 0-shell_main.c
@@ -12,6 +12,8 @@ int main (int argc __attribute__((unused)),
 		char **argv)
 {
 	char *line;
+	char **args;
+	int cmd_type;
 
 	(void) argv;
 	
@@ -21,5 +23,15 @@ int main (int argc __attribute__((unused)),
 	{
 		print(" ($) ", STDOUT_FILENO);
 		line = _getline();
+		
+		if (line == NULL)
+		{
+			if (isatty(STDIN_FILENO))
+				break;
+		}
+		args = tokenize(line, DELIM);
+		cmd_type = check_command(args[0]);
+		shell_execute(args, cmd_type);
 	}
+	return (1);
 }
